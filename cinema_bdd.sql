f641dd2ecdb4cdc2cbb07376f7c8b07b144461b1
Jumuse <justinehoguet@gmail.com>
2023-03-25 13:34:12 +0100

    adding customers class and adding foreign keys

--- cinema_bdd.sql
+++ cinema_bdd.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE IF NOT EXISTS bdd_cinema;
 
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(30) NOT NULL,
+    firstname VARCHAR(30),
+    lastname VARCHAR(30) NOT NULL,
+    age INT(2) NOT NULL,
+    is_student BOOLEAN
+);
+
 CREATE TABLE rooms (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -25,6 +34,15 @@ CREATE TABLE movies (
     current_week BOOLEAN NOT NULL
 );
 
+CREATE TABLE customers (
+    users_id INT(11) NOT NULL PRIMARY KEY,
+    /*One to Many between customers and movies*/
+    watches INT(11) NOT NULL,
+    FOREIGN KEY (watches) REFERENCES movies(id),
+    /*heritance between users and customers*/
+    FOREIGN KEY(users_id) REFERENCES users(id)
+);
+
 CREATE TABLE cinemas_admin (
     /*fusion between 'admin and cinemas'*/
     admin_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -34,19 +52,6 @@ CREATE TABLE cinemas_admin (
     closing_time TIME NOT NULL
 );
 
-
-CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(30) NOT NULL,
-    firstname VARCHAR(30),
-    lastname VARCHAR(30) NOT NULL,
-    age INT(2) NOT NULL,
-    is_student BOOLEAN,
-    watches INT(11) NOT NULL,
-  /*One to Many between users and movies*/
-    FOREIGN KEY (watches) REFERENCES movies(id)
-);
-
 CREATE TABLE tickets (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     date DATE NOT NULL,
@@ -54,18 +59,20 @@ CREATE TABLE tickets (
     room INT(2) NOT NULL,
     price FLOAT NOT NULL,
     is_used BOOLEAN NOT NULL,
+    /*One to Many association between customers and tickets*/
     pays_for INT(11) NOT NULL,
-    FOREIGN KEY (pays_for) REFERENCES users(id),
+    FOREIGN KEY (pays_for) REFERENCES customers(users_id),
+    /*One to Many association between tickets and sessions*/
     books_a INT(11) NOT NULL,
     FOREIGN KEY (books_a) REFERENCES sessions(id)
 );
 
 CREATE TABLE payments (
-    /*associative table between users and tickets*/
+    /*associative table between customers and tickets*/
     tickets_id INT(11) NOT NULL,
-    users_id INT(11) NOT NULL,
-    PRIMARY KEY (users_id, tickets_id),
-    FOREIGN KEY (users_id) REFERENCES users(id),
+    customers_id INT(11) NOT NULL,
+    PRIMARY KEY (customers_id, tickets_id),
+    FOREIGN KEY (customers_id) REFERENCES customers(users_id),
     FOREIGN KEY (tickets_id) REFERENCES tickets(id),
     total FLOAT NOT NULL,
     is_onsite BOOLEAN NOT NULL
